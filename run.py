8e1594d00e0817c79293a5330587fe579f5f8050
Vishal Chandra <chapavishal3@gmail.com>
2024-09-10 00:41:33 +0000

    Calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-import sys, subprocess
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'gspread'])
+# import sys, subprocess
+# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'gspread'])
 import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
@@ -17,12 +17,12 @@ SHEET = GSPREAD_CLIENT.open('Delish_n_Wholesome')
 
 def get_sales_data():
     """
-    Get sales figures input from the user 
+    Get sales figures input from the user. 
     Run a while loop to collect a valid string of data from the user via the terminal, which must be a string of 6 numbers seperated by commas. The loop will repeatedly request data, until it is valid
     """    
     while True:
         print("Please enter sales data from the last market.")
-        print("Data should be six numbers, seperated by commas.")
+        print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
@@ -40,7 +40,7 @@ def validate_data(values):
     """
     Inside the try, converts all string values into integers. Raises ValueError if strings cannot be converted into int, or if there aren't exactly 6 values.
     """
-    print(values)
+   
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -52,24 +52,6 @@ def validate_data(values):
         return False
     
     return True
-
-# def update_sales_worksheet(data):
-#     """
-#     update sales worksheet, add new row with the list data provided.
-#     """
-#     print("Updating sales worksheet...\n")
-#     sales_worksheet = SHEET.worksheet("sales")
-#     sales_worksheet.append_row(data)
-#     print("Sales worksheet updated successfully.\n")
-
-# def update_surplus_worksheet(data):
-#     """
-#     update surplus worksheet, add new row with the list data provided.
-#     """
-#     print("Updating surplus worksheet...\n")
-#     surplus_worksheet = SHEET.worksheet("surplus")
-#     surplus_worksheet.append_row(data)
-#     print("Surplus worksheet updated successfully.\n")
 
 def update_worksheet(data, worksheet):
     """
@@ -84,8 +66,8 @@ def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
     The surplus is defined as the sales figure subtracted from the stock:
-    -Positive surplis indictes waste
-    -Negative surplus indicates extra made when stock was solid out.
+    - Positive surplis indictes waste
+    - Negative surplus indicates extra made when stock was sold out.
     """
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
@@ -110,8 +92,24 @@ def get_last_5_entries_sales():
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
-        columns.append(column)
+        columns.append(column[-5:])
     return columns
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    
+    return new_stock_data
 
 def main():
     """
@@ -121,8 +119,11 @@ def main():
     sales_data = [int(num) for num in data]
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data, "surplus")
+    update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to Delish Wholesome Data Automation")
-#main()
-sales_columns = get_last_5_entries_sales()
+main()
+
